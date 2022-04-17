329fae5dc5e64055354e83077e1d375896996010
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2022-04-17 19:35:34 +0530

    Assertions on JSON Response body , created separate payload file

--- src/test/java/googleApis/addLocation.java
+++ src/test/java/googleApis/addLocation.java
@@ -1,8 +1,9 @@
 package googleApis;
 
 import io.restassured.RestAssured;
-
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import files.payLoads;
 
 public class addLocation {
     public static void main(String[] args) {
@@ -13,24 +14,10 @@ public class addLocation {
 
         RestAssured.baseURI= "https://rahulshettyacademy.com";
         given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Tanmay Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29 side layout cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}\n" +
-                        "\n")
+                .body(payLoads.addPlace())
                 .when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200);
+                .then().log().all().assertThat().statusCode(200)
+                .body("scope",equalTo("APP"))
+                .header("Server","Apache/2.4.41 (Ubuntu)");
     }
 }
