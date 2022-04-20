98c033ce530d108721243ac5e5dcf0111d5f30b7
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2022-04-20 09:46:52 +0530

    Parsing the Json Response body using JsonPath class

--- src/test/java/googleApis/addLocation.java
+++ src/test/java/googleApis/addLocation.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import files.payLoads;
+import io.restassured.path.json.JsonPath;
 
 public class addLocation {
     public static void main(String[] args) {
@@ -13,11 +14,19 @@ public class addLocation {
          */
 
         RestAssured.baseURI= "https://rahulshettyacademy.com";
-        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+       String response = given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
                 .body(payLoads.addPlace())
                 .when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200)
+                .then().assertThat().statusCode(200)
                 .body("scope",equalTo("APP"))
-                .header("Server","Apache/2.4.41 (Ubuntu)");
+                .header("Server","Apache/2.4.41 (Ubuntu)")
+                .extract().response().asString();
+
+                System.out.println(response);
+                //Parse the Json file, i.e extract values from the response
+                JsonPath parsedResponse = new JsonPath(response);
+                String placeid = parsedResponse.getString("place_id");
+                System.out.println("Extracted Place id from Response"+ placeid);
+
     }
 }
