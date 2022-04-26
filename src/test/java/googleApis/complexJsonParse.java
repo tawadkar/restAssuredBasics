25177ff6436b48b9b48287791a8d1333a9afeec6
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2022-04-26 08:26:10 +0530

    Added complex Json File

---
+++ src/test/java/googleApis/complexJsonParse.java
@@ -0,0 +1,10 @@
+package googleApis;
+
+import io.restassured.path.json.JsonPath;
+import files.payLoads;
+
+public class complexJsonParse {
+
+    JsonPath complexjson = new JsonPath(payLoads.CoursePrice());
+
+}
