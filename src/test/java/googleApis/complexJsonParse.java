1ada043c8f6bd4644665ecc3f20523aaa6b17971
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2022-04-27 08:00:48 +0530

    Retrieving Json array size and its elements

--- src/test/java/googleApis/complexJsonParse.java
+++ src/test/java/googleApis/complexJsonParse.java
@@ -1,10 +1,25 @@
 package googleApis;
 
+import groovy.json.JsonOutput;
 import io.restassured.path.json.JsonPath;
 import files.payLoads;
 
 public class complexJsonParse {
+    public static void main(String[] args) {
 
-    JsonPath complexjson = new JsonPath(payLoads.CoursePrice());
 
+        JsonPath complexjson = new JsonPath(payLoads.CoursePrice());
+
+        //No of courses returned by API
+        int course = complexjson.getInt("courses.size()");
+        System.out.println(course);
+
+        // Purchase amount
+        int amount = complexjson.getInt("dashboard.purchaseAmount");
+        System.out.println(amount);
+
+        //Title of first course
+       String title = complexjson.get("courses[2].title");
+        System.out.println(title);
+    }
 }
