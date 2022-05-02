faaa104b0f81c037ff682dd2dd03600b634193b5
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2022-05-02 07:54:18 +0530

    Parameterization of API Tests with multiple data sets

--- src/test/java/libraryApis/dynamicJson.java
+++ src/test/java/libraryApis/dynamicJson.java
@@ -2,6 +2,7 @@ package libraryApis;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import files.payLoads;
 import googleApis.commonUtils;
@@ -9,13 +10,13 @@ import static io.restassured.RestAssured.given;
 
 public class dynamicJson {
 
-    @Test
+    @Test(dataProvider = "BooksData")
 
-    public void addBook(){
+    public void addBook(String isbn,String aisle){
         RestAssured.baseURI= "http://216.10.245.166";
 
         String response = given().log().all().header("Content-Type","application/json")
-                .body(payLoads.addBook("2532","absd"))
+                .body(payLoads.addBook(isbn,aisle))
                 .when().post("/Library/Addbook.php")
                 .then().log().all().assertThat().statusCode(200)
                 .extract().response().asString();
@@ -24,6 +25,14 @@ public class dynamicJson {
         String id = book.get("ID");
         System.out.println(id);
 
+    }
+
+    @DataProvider(name="BooksData")
+    //array= collection of elements
+    //multidimensional array = collection of arrays
+    public Object [][] getData(){
+
+       return new Object [][]{{"qwer","357"},{"mnas","987"},{"opnw","182"}};
     }
 
 }
